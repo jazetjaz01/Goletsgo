afcf14d60622394aad25d0d08a49f663a26b6342
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2024-12-01 10:14:18 +0100

    update expo and router

---
+++ src/app/index.tsx
@@ -0,0 +1,8 @@
+import { Text } from "react-native";
+
+
+export default function HomeScreen() {
+    return (
+        <Text className="font-bold color-slate-700 ">Mon texte dimanche</Text>
+    )
+}
